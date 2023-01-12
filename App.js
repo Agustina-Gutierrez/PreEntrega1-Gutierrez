5e968394f9b9bc23fa47072c5da279cf3b424a04
Agustina-Gutierrez <61707564+Agustina-Gutierrez@users.noreply.github.com>
2023-01-11 23:33:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar.jsx';
 import Greeting from './components/ItemListContainer/ItemListContainer.jsx';
+import Detail from './components/ItemListContainer/ItemDetailContainer.jsx';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -11,10 +12,13 @@ export default function App() {
       <NavBar />
       <Routes>
        <Route path="/" element={<Greeting saludo='Bienvenido a Amar Maderas'/>} />
+       <Route path="/detalle/:id" element={<Detail/>} />
        <Route path="*" element={<h2>Pagina no encontrada</h2>}/>
       </Routes>
     </Router>
     </>
   );
 }
+
+
 
